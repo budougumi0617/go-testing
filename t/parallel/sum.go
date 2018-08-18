342011dfe2452b1198713c6985b1db351c04a9d0
budougumi0617 <budougumi0617@gmail.com>
2018-08-18 11:20:21 +0900

    :+1: Add parallel test

---
+++ t/parallel/sum.go
@@ -0,0 +1,6 @@
+package parallel
+
+// Sum returns two int values.
+func Sum(a, b int) int {
+	return a + b
+}
---
+++ t/parallel/sum_test.go
@@ -0,0 +1,27 @@
+package parallel_test
+
+import (
+	"testing"
+
+	"github.com/budougumi0617/go-testing/t/parallel"
+)
+
+func TestSum(t *testing.T) {
+	tests := []struct {
+		name       string
+		a, b, want int
+	}{
+		{"Simple", 0, 1, 1},
+		{"Minus", -1, -1, -2},
+		{"Both", -3, 2, -1},
+	}
+	for _, tt := range tests {
+		tt := tt // Don't forget when parallel test
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if got := parallel.Sum(tt.a, tt.b); got != tt.want {
+				t.Fatalf("want = %d, got = %d", tt.want, got)
+			}
+		})
+	}
+}
