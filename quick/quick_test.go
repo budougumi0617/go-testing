0e1a3dfc84e9a56d8065a1ca4db4edacd2dac73e
budougumi0617 <budougumi0617@gmail.com>
2018-09-10 08:10:00 +0900

    :+1: Use quick.Config

--- quick/quick_test.go
+++ quick/quick_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"math/rand"
+	"reflect"
 	"testing"
 	"testing/quick"
 )
@@ -19,7 +21,22 @@ func TestDiv(t *testing.T) {
 	f := func(x, y MyInt) bool {
 		return div(x, y) == (x.V / y.V)
 	}
-	if err := quick.Check(f, nil); err != nil {
+
+	// Set config
+	cfg := &quick.Config{
+		// 試行回数
+		MaxCount: 10,
+		// Seed
+		Rand: rand.New(rand.NewSource(2)),
+		// 独自定義した引数の生成関数
+		Values: func(args []reflect.Value, rand *rand.Rand) {
+			args[0] = reflect.ValueOf(MyInt{rand.Int(), "x"})
+			args[1] = reflect.ValueOf(MyInt{rand.Int(), "y"})
+		},
+	}
+
+	if err := quick.Check(f, cfg); err != nil {
+		// Use specified error type
 		if ce, ok := err.(*quick.CheckError); ok {
 			t.Errorf("Try count = %d, In %#v, Out %s\n", ce.Count, ce.In, ce)
 		} else {
